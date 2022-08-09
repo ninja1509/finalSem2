31a42a03b2fe957fa48ff3c4348b84d62adae9c8
ninja1509 <63071772+ninja1509@users.noreply.github.com>
2022-08-09 22:02:44 +0530

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -77,7 +77,7 @@ class Lesson(models.Model):
 
 
 # Enrollment model
-# <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
+# Once a user enrolled a class, an enrollment entry should be created between the user and course
 # And we could use the enrollment to track information such as exam submissions
 class Enrollment(models.Model):
     AUDIT = 'audit'
@@ -95,16 +95,12 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
-# <HINT> Create a Question Model with:
+#  Question Model with:
     # Used to persist question content for a course
     # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
     # Has a grade point for each question
     # Has question content
-    # Other fields and methods you would like to design
-#class Question(models.Model):
-    # Foreign key to lesson
-    # question text
-    # question grade/mark
+
 class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
@@ -113,7 +109,7 @@ class Question(models.Model):
     def __str__(self):
         return "Question: " + self.content
 
-    # <HINT> A sample model method to calculate if learner get the score of the question
+    # A sample model method to calculate if learner get the score of the question
     def is_get_score(self, selected_ids):
         all_answers = self.choice_set.filter(is_correct=True).count()
         selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
@@ -121,7 +117,7 @@ class Question(models.Model):
             return True
         else:
             return False
-    # <HINT> A sample model method to calculate if learner get the score of the question
+    
     #def is_get_score(self, selected_ids):
     #    all_answers = self.choice_set.filter(is_correct=True).count()
     #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
@@ -131,7 +127,7 @@ class Question(models.Model):
     #        return False
 
 
-#  <HINT> Create a Choice Model with:
+# a Choice Model with:
     # Used to persist choice content for a question
     # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
     # Choice content
@@ -142,7 +138,7 @@ class Choice(models.Model):
     content = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
-# <HINT> The submission model
+# The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
